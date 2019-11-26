622f1879ebb8bbd1e46f6b5446de0ab2f5adee93
alsanchez86 <sanchezpizarro86@gmail.com>
2019-11-26 16:09:12 +0100

    manage assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import image from './image.png';
 
 function component() {
     const element = document.createElement('div');
+    const myImage = new Image();
+    // Feed element
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    // Add the image to our existing div.
+    myImage.src = image;
+    element.appendChild(myImage);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,24 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-            ],
-        }, ],
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
     },
 };
