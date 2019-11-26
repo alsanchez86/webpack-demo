3de9e76dcf8bf59584277f19b9ee42eb251ecd0b
alsanchez86 <sanchezpizarro86@gmail.com>
2019-11-26 18:52:08 +0100

    add print function

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import _ from 'lodash';
 import './style.css';
 import image from './image.png';
 import data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
     const myImage = new Image();
-    // Feed element
+    // Add custom text
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    // Button
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     // Add the image to our existing div.
     myImage.src = image;
     element.appendChild(myImage);
+
     return element;
 }
 
